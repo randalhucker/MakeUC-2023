b16d47a3a89ce8dd160f8a25ded0177211afe858
Ethan Chaplin <ethan.p.chaplin@gmail.com>
2023-11-04 13:23:25 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+import Generator from './components/Generator.vue'
+import EventBus from './services/event-bus';
+
+const tick = (timestamp: DOMHighResTimeStamp) => {
+  window.requestAnimationFrame(tick);
+  EventBus.emit('tick');
+}
+
+onMounted(() => {
+  window.requestAnimationFrame(tick);
+})
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+  <Generator />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/Generator.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref, onBeforeMount } from 'vue';
+import EventBus from '../services/event-bus';
+
+const loadROC = ref(0.0);
+const currentLoad = ref(0.0);
+
+
+const calculateFrequency = () => {
+
+};
+
+onBeforeMount(() => {
+    EventBus.on('tick', () => {
+        console.log('tick');
+    });
+})
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
